7201dad60cf082aa2708a7c77daa8fbf339db2e7
moht12932 <moht1293.25@gmail.com>
2025-02-11 14:31:46 +0530

    c1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-};
-
-export default nextConfig;
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: process.env.NODE_ENV === "production" ? "/my-chat-app/" : "",
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
